fae7b3c16b59cefc230a5646430610ed40078d76
varun13125 <varun@kraftmortgages.ca>
2025-04-22 12:22:34 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
